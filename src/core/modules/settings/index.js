788c91ed25c6592379ee453db4d06c58d5e10e3c
besik007 <west.south190011@gmail.com>
2018-06-22 10:28:17 +0700

    delete setting module

--- src/core/modules/settings/index.js
+++
@@ -1,4 +0,0 @@
-export { default as settings } from './reducer';
-export { connectSettings } from './connectSettings';
-export { settingsActionCreators } from './actions';
-export { default as settingsSaga } from './saga';
